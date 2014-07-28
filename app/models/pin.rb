d11f9296e92c5c679e6716fdd849c146bbda42e9
Oliver Jones <oapjones@gmail.com>
2014-07-28 21:34:54 +0200

    pin.rb 4

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,5 +6,5 @@ class Pin < ActiveRecord::Base
             :styles => { :medium => "x300", :thumb => "x100" },
             :storage => :s3,
             :bucket => ENV['S3_BUCKET_NAME'],
-            :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+            :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY']
 end
