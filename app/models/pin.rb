189c310723ec6932d2db89f25e060a674b34d15d
Oliver Jones <oapjones@gmail.com>
2014-07-28 21:25:18 +0200

    change pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,9 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	has_attached_file :image,
+            :styles => { :medium => "x300", :thumb => "x100" },
+            :storage => :s3,
+            :bucket => '******',
+            :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 end
