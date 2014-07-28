5f67eea885c59db1281931bc7bb1d1a81468eaad
Oliver Jones <oapjones@gmail.com>
2014-07-28 22:37:50 +0200

    test debugging 1

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,9 +2,5 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
-	has_attached_file :image,
-            :styles => { :medium => "x300", :thumb => "x100" },
-            :storage => :s3,
-            :bucket => ENV['S3_BUCKET_NAME'],
-            :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY']
+	
 end
